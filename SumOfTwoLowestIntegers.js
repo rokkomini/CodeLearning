b99b1af79e586b852df6c82d6faf08f7631f9ca5
Mika Hägerström <mikahagerstrom@hotmail.com>
2022-03-27 20:21:10 +0200

    nicer code

--- SumOfTwoLowestIntegers.js
+++ SumOfTwoLowestIntegers.js
@@ -1,11 +1,17 @@
 const array = [19, 5, 42, 2, 77];
 
-function sumTwoSmallestNumbers(numbers) {  
+/* function sumTwoSmallestNumbers(numbers) {  
     const sortedArray = numbers.sort(function (a, b) {return a - b});
     const firstTwoNumbers = sortedArray.slice(0,2)
     const firstNumber = firstTwoNumbers[0]
     const secondNumber = firstTwoNumbers[1]
     return firstNumber + secondNumber
+  } */
+
+  function sumTwoSmallestNumbers(numbers) {  
+    const sortedArray = numbers.sort(function (a, b) {return a - b});
+    const firstTwoNumbers = sortedArray.slice(0,2)
+    return firstTwoNumbers[0] + firstTwoNumbers[1]
   }
 
   console.log(sumTwoSmallestNumbers(array))
